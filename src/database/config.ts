63e1b69275cc99fbb249be171fd0e8a76060f5c8
Sumit Grover <sumitgrover108@gmail.com>
2025-06-07 19:17:43 +0530

    prod ready

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,18 +1,27 @@
 import { envConfig } from '@/config/env.config';
+import { ChartInfo } from '@/entities/chartInfo.entity';
+import { Datasource } from '@/entities/datasource.entity';
+import { User } from '@/entities/user.entity';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { join } from 'path';
 
 const databaseConfig: DataSourceOptions = {
   type: 'postgres',
   host: envConfig.DB_HOST,
-  port: Number(envConfig.DB_PORT),
+  port: envConfig.DB_PORT,
   username: envConfig.DB_USERNAME,
   password: envConfig.DB_PASSWORD,
   database: envConfig.DB_DATABASE,
   synchronize: false,
   logging: false,
-  entities: ['src/entities/**/*.entity{.ts,.js}'],
-  migrations: ['src/database/migrations/**/*{.ts,.js}'],
-  ssl: envConfig.DB_SSL ? { rejectUnauthorized: false } : false
+  connectTimeoutMS: 0,
+  entities: [User, ChartInfo, Datasource],
+  migrations: [join(__dirname, '../database/migrations/**/*{.ts,.js}')],
+  ssl: envConfig.DB_SSL
+    ? {
+        rejectUnauthorized: false
+      }
+    : false
 };
 
 const AppDataSource = new DataSource(databaseConfig);
